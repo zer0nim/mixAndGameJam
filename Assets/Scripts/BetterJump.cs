64cfca05f6751eecc2571f75de03b76292c61f4c
Ernest Marin <emarin@student.42.fr>
2019-10-12 16:21:26 +0200

    detect ground before jump

---
+++ Assets/Scripts/BetterJump.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BetterJump : MonoBehaviour
+{
+	public Rigidbody2D	rb;
+    [Range(1, 10)]
+	public float		fallMultiplier = 2.5f;
+	[Range(1, 10)]
+	public float		lowJumpMultiplier = 2f;
+
+    void FixedUpdate() {
+        // Better jumping
+		if (rb.velocity.y < 0) {
+			rb.gravityScale = fallMultiplier;
+		} else if (rb.velocity.y > 0 && !Input.GetButton("Jump")) {
+			rb.gravityScale = lowJumpMultiplier;
+		} else {
+            rb.gravityScale = 1f;
+        }
+    }
+}
--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -5,47 +5,58 @@ using UnityEngine;
 public class MovementController : MonoBehaviour
 {
 	public Rigidbody2D	rb;
-	public float		moveSpeed = 10f;
+	public float		moveSpeed = 8f;
+	// jump section
 	[Range(1, 10)]
 	public float		jumpVelocity = 5f;
-	[Range(1, 10)]
-	public float		fallMultiplier = 2.5f;
-	[Range(1, 10)]
-	public float		lowJumpMultiplier = 2f;
+	public float		groundedHeight = 0.2f;
+
+	public LayerMask	groundMask;
 
 	Vector2	_moveDir;
+	// jump section
+	bool	_jumpRequest = false;
+	bool	_grounded = false;
+	Vector2 _playerSize;
+	Vector2	_boxSize;
 
-	// Update is called once per frame
+
+	void	Awake() {
+		_playerSize = GetComponent<BoxCollider2D>().size;
+		_boxSize = new Vector2(_playerSize.x, groundedHeight);
+	}
+
 	void    Update() {
 		_moveDir.x = Input.GetAxis("Horizontal");
 		_moveDir.y = Input.GetAxis("Vertical");
-		// movement = Vector2.ClampMagnitude(movement, 1);
+		_moveDir = Vector2.ClampMagnitude(_moveDir, 1);
 
-		if (Input.GetButtonDown("Jump"))
-			jump();
+		if (Input.GetButtonDown("Jump") && _grounded)
+			_jumpRequest = true;
 	}
 
 
 	void	FixedUpdate() {
 		walk();
 
-		// Better jumping
-		if (rb.velocity.y < 0) {
-			rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.fixedDeltaTime;
-		} else if (rb.velocity.y > 0 && !Input.GetButton("Jump")) {
-			rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.fixedDeltaTime;
+		if (_jumpRequest)
+			jump();
+		else {
+			Vector2 boxCenter = (Vector2)transform.position + Vector2.down * (_playerSize.y + _boxSize.y) * 0.5f;
+			_grounded = (Physics2D.OverlapBox(boxCenter, _boxSize, 0f, groundMask) != null);
 		}
 	}
 
 
 	void	walk() {
 		rb.velocity = new Vector2(_moveDir.x * moveSpeed, rb.velocity.y);
-		// rb.velocity = new Vector2(
-		// 	_moveDir.x * moveSpeed * Time.fixedDeltaTime, rb.velocity.y);
 	}
 
 	void	jump() {
 		rb.velocity = new Vector2(rb.velocity.x, 0);
 		rb.velocity += Vector2.up * jumpVelocity;
+
+		_jumpRequest = false;
+		_grounded = false;
 	}
 }
